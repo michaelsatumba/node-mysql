0dfd83f6736b1a4734556ad7c71fc272f7a01ace
michaelsatumba <mjsatumba@gmail.com>
2022-09-27 14:33:27 -0700

    add .env and cloud mysql

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var dt = require('./datemodule');
 var url = require('url');
 var fs = require('fs');
 var mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
 
 var express = require('express');
 const path = require('path');
@@ -18,70 +20,81 @@ app.get('/styles.css', function (req, res) {
 });
 
 var con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '123',
+	// host: 'localhost',
+	// user: 'root',
+	// password: '123',
+	host: `${process.env.HOST}`,
+	user: `${process.env.USER}`,
+	password: `${process.env.PASSWORD}`,
+	database: `${process.env.DATABASE}`,
 });
 
-let sqlRes = 'nothing';
-con.connect(function (err) {
-	if (err) throw err;
-	console.log('Connected!');
-	con.query(`SELECT * FROM sakila.actor LIMIT 100`, function (err, res) {
-		if (err) throw err;
-		sqlRes = res;
-		// console.log(sqlRes);
-		console.log(
-			sqlRes.map(function (element) {
-				return (
-					'<p>' +
-					element.actor_id +
-					' ' +
-					element.first_name +
-					' ' +
-					element.last_name +
-					' ' +
-					element.last_update.getFullYear() +
-					'</p>'
-				);
-			})
-		);
-		// console.log(res[0].first_name); // woo!
-		fs.writeFile(
-			'mynewfile1.html',
-			`
-			<!DOCTYPE html>
-			<html>
-			<head>
-  <link rel="stylesheet" type="text/css" href="./styles.css">
-</head>
-        <body>
-        <h1 style="color:blue;">Actors in 2006</h1>
-        <div>
-        ${sqlRes
-					.map(function (element) {
-						return (
-							'<p>' +
-							element.actor_id +
-							' ' +
-							element.first_name +
-							' ' +
-							element.last_name +
-							' ' +
-							'</p>'
-						);
-					})
-					.join('')}
-        <div>
-        </body>
-        </html>`,
-			function (err) {
-				if (err) throw err;
-				console.log('Updated!');
-			}
-		);
-	});
+con.connect();
+
+con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+	if (error) throw error;
+	console.log('The solution is: ', results[0].solution);
 });
+
+// let sqlRes = 'nothing';
+// con.connect(function (err) {
+// 	if (err) throw err;
+// 	console.log('Connected!');
+// 	con.query(`SELECT * FROM sakila.actor LIMIT 100`, function (err, res) {
+// 		if (err) throw err;
+// 		sqlRes = res;
+// 		// console.log(sqlRes);
+// 		console.log(
+// 			sqlRes.map(function (element) {
+// 				return (
+// 					'<p>' +
+// 					element.actor_id +
+// 					' ' +
+// 					element.first_name +
+// 					' ' +
+// 					element.last_name +
+// 					' ' +
+// 					element.last_update.getFullYear() +
+// 					'</p>'
+// 				);
+// 			})
+// 		);
+// 		// console.log(res[0].first_name); // woo!
+// 		fs.writeFile(
+// 			'mynewfile1.html',
+// 			`
+// 			<!DOCTYPE html>
+// 			<html>
+// 			<head>
+//   <link rel="stylesheet" type="text/css" href="./styles.css">
+// </head>
+//         <body>
+//         <h1 style="color:blue;">Actors in 2006</h1>
+//         <div>
+//         ${sqlRes
+// 					.map(function (element) {
+// 						return (
+// 							'<p>' +
+// 							element.actor_id +
+// 							' ' +
+// 							element.first_name +
+// 							' ' +
+// 							element.last_name +
+// 							' ' +
+// 							'</p>'
+// 						);
+// 					})
+// 					.join('')}
+//         <div>
+//         </body>
+//         </html>`,
+// 			function (err) {
+// 				if (err) throw err;
+// 				console.log('Updated!');
+// 			}
+// 		);
+// 	});
+// });
 
 app.use('/', router);
 app.listen(3000);
