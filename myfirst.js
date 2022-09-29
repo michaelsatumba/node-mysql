76c6aabdb3b7dbbe4fe52469721ff642772dbeef
michaelsatumba <mjsatumba@gmail.com>
2022-09-29 10:59:12 -0700

    formidable - fs - upload file

--- myfirst.js
+++ myfirst.js
@@ -200,3 +200,83 @@ eventEmitter.on('scream', myEventHandler);
 //Fire the 'scream' event:
 eventEmitter.emit('screm');
 */
+
+// Create a Node.js file that writes an HTML form, with an upload field:
+/*
+http
+	.createServer(function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write(
+			'<form action="fileupload" method="post" enctype="multipart/form-data">'
+		);
+		res.write('<input type="file" name="filetoupload"><br>');
+		res.write('<input type="submit">');
+		res.write('</form>');
+		return res.end();
+	})
+	.listen(8080);
+*/
+
+//Include the Formidable module to be able to parse the uploaded file once it reaches the server.
+
+// When the file is uploaded and parsed, it gets placed on a temporary folder on your computer.
+/*
+http
+	.createServer(function (req, res) {
+		if (req.url == '/fileupload') {
+			var form = new formidable.IncomingForm();
+			form.parse(req, function (err, fields, files) {
+				res.write('File uploaded');
+				res.end();
+			});
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(
+				'<form action="fileupload" method="post" enctype="multipart/form-data">'
+			);
+			res.write('<input type="file" name="filetoupload"><br>');
+			res.write('<input type="submit">');
+			res.write('</form>');
+			return res.end();
+		}
+	})
+	.listen(8080);
+*/
+
+// When a file is successfully uploaded to the server, it is placed on a temporary folder.
+
+// The path to this directory can be found in the "files" object, passed as the third argument in the parse() method's callback function.
+
+// To move the file to the folder of your choice, use the File System module, and rename the file:
+/*
+http
+	.createServer(function (req, res) {
+		if (req.url == '/fileupload') {
+			var form = new formidable.IncomingForm();
+			form.parse(req, function (err, fields, files) {
+				var oldpath = files.filetoupload.filepath;
+				var newpath =
+					// 'C:\Users\Kuya\Desktop' + //adds this as string
+					// 'C:/Users/Kuya/Desktop/' + // adds file to desktop
+					'C:/Users/Kuya/Desktop/node-mysql/'  +
+					files.filetoupload.originalFilename;
+
+				fs.rename(oldpath, newpath, function (err) {
+					if (err) throw err;
+					res.write('File uploaded and moved!');
+					res.end();
+				});
+			});
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(
+				'<form action="fileupload" method="post" enctype="multipart/form-data">'
+			);
+			res.write('<input type="file" name="filetoupload"><br>');
+			res.write('<input type="submit">');
+			res.write('</form>');
+			return res.end();
+		}
+	})
+	.listen(8080);
+	*/
